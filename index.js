0be5012f870e921431089e39354f05e9d7c147b8
Yami <yamikuronue@gmail.com>
2015-07-27 20:37:11 -0400

    Adding console control. This should work, but does not.

--- index.js
+++ index.js
@@ -1,39 +1,75 @@
-var bot = require("sockbot");
+var  bot = require("sockbot");
 var botVersion = bot.version;
 var botConfigHelper = require('./node_modules/sockbot/config');
 var Path = require('path');
-
 var Hapi = require('hapi');
 
+var currCommand = "";
+var server;
+
+var stdin = process.openStdin();
+stdin.on('data', processInput);
+console.log("Welcome to SockMon! You are currently configured to use the default config. If you want to get started, type 'start'. To change the config file, try 'config'. To exit, type 'exit'. ");
+
+function processInput(data) {
+	data = data.toString("utf-8").trim();
+	if (!currCommand) {
+		if (data.toLowerCase === "start") {
+			if (!server) startServer();
+			else console.log("Server already listening on port 8000!");
+		} else if (data.toLowerCase === "config") {
+			currCommand = "config";
+			console.log("Enter config file: ");
+		} else if (data.toLowerCase === "stop") {
+			if (!server) console.log("Server is not running!");
+			stopServer();
+		} else if (data.toLowerCase === "exit") {
+			console.log("Goodbye!");
+			process.exit()
+		} else {
+			console.log("Unknown command: '" + data.toLowerCase() + "'")
+		}
+	}
+}
+
+
+function startServer()  {
+	server = new Hapi.Server();
+	server.connection({ 
+		host: 'localhost', 
+		port: 8000 
+	});
+
+	server.views({
+		engines: {
+			html: require('handlebars')
+		},
+		path: Path.join(__dirname, 'templates')
+	});
+
+	// Add the route
+	server.route({
+		method: 'GET',
+		path:'/', 
+		handler: function (request, reply) {
+			reply.view('index', {
+				version: botVersion,
+				username: botConfigHelper.core.username,
+				owner: botConfigHelper.core.owner,
+				forum: botConfigHelper.core.forum,
+				plugins: Object.keys(botConfigHelper.plugins)
+			});
+		}
+	});
+
+	// Start the server
+	server.start();
+}
+
+function stopServer() {
+	server.stop();
+	server = null;
+}
+
+
 // Create a server with a host and port
-var server = new Hapi.Server();
-server.connection({ 
-    host: 'localhost', 
-    port: 8000 
-});
-
-server.views({
-    engines: {
-        html: require('handlebars')
-    },
-    path: Path.join(__dirname, 'templates')
-});
-
-// Add the route
-server.route({
-    method: 'GET',
-    path:'/', 
-    handler: function (request, reply) {
-        reply.view('index', {
-			version: botVersion,
-			username: botConfigHelper.core.username,
-			owner: botConfigHelper.core.owner,
-			forum: botConfigHelper.core.forum,
-			plugins: Object.keys(botConfigHelper.plugins)
-		});
-    }
-});
-
-// Start the server
-//bot.start();
-server.start();
